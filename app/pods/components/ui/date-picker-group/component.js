0b3ec8535aa39860cb269ba28bb3435ef9455560
Chris Westra <cwestra@gmail.com>
2015-12-29 16:50:24 -0500

    date picker group start

--- app/pods/components/ui/date-picker-group/component.js
+++ app/pods/components/ui/date-picker-group/component.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
+import moment from 'moment';
 
-export default Ember.Component.extend({
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  tagName: '',
+  startDateObj: null,
+  endDateObj: null,
+
+  init() {
+    this._super(...arguments);
+
+    let start = moment();
+    let end = start.clone().add(7, 'days');
+
+    this.setProperties({startDateObj: start, endDateObj: end});
+
+  },
+
+  startDate: computed('startDateObj', function() {
+    return this.get('startDateObj').format('l');
+  }),
+
+  endDate: computed('endDateObj', function() {
+    return this.get('endDateObj').format('l');
+  }),
+
+  actions: {
+    dateChanged(propName, newDate) {
+      return null;
+    }
+  }
+
 });
